1240c5f8467949a27d6cca9d0ff012cd0a252099
Bhavnameshram <bhawanameshram1910@gmail.com>
2025-12-30 16:44:08 +0530

    changes3

--- Client/src/Pages/Bhavna.jsx
+++ Client/src/Pages/Bhavna.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 function Bhavna() {
   return (
-    <div>
-      hey i m bhavna
-    </div>
+    <>
+   <div>
+    hey i am bhavna
+    <li>Pratik</li>
+    <li>Piyush</li>
+    <li>Devyani/steffi</li>
+    <li>Bhavna</li>
+    <li>Dhanasvi</li>
+    <li>Minal</li>
+    <li>Akash</li>
+   </div>
+   </>
   )
 }
 
