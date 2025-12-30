a443f10211a909512572bca4934b195f6420f813
devyanis05 <shelkegaju700@gmail.com>
2025-12-30 16:44:50 +0530

    Navbar imported

--- Client/src/Pages/Devyani_Steffi.jsx
+++ Client/src/Pages/Devyani_Steffi.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
 
 function Devyani_Steffi() {
   return (
     <>
+        <Navbar />
         <div>Devyani_Steffi</div>
     </>
   )
